51147d484d6d531e92eda1cbb5e95fd26b401b0a
vvzababurin <vvzababurin@gmail.com>
2024-05-27 01:48:07 +0300

    update

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 # export EMSCRIPTENDIR=c:/emscripten/emsdk
 
 export CC=emcc
-export EMCCFLAGS="-s SINGLE_FILE=0 -s TOTAL_MEMORY=200MB -s ALLOW_MEMORY_GROWTH=0 -s EXPORTED_RUNTIME_METHODS=['callMain','ccall','cwrap'] -s INVOKE_RUN=1 -O3"
+export EMCCFLAGS="-s SINGLE_FILE=0 -s TOTAL_MEMORY=200MB -s ALLOW_MEMORY_GROWTH=0 -s EXPORTED_RUNTIME_METHODS=['callMain','ccall','cwrap'] -s INVOKE_RUN=0 -O3"
 
 cd src
 export DIR=`pwd`
